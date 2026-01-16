4c7560323cbd4a51722e5986dc8aac6a2092d447
fredissy <frl@filmstory.fr>
2026-01-16 18:18:03 +0100

    Fix "gevent-websocket server is not configured appropriately"

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set -eu
 echo "Starting Shots Poker"
 echo "Version: ${APP_VERSION:-unknown}"
 
-exec gunicorn --worker-class gevent -w 1 --bind 0.0.0.0:5000 wsgi:app
+exec gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 --bind 0.0.0.0:5000 wsgi:app
